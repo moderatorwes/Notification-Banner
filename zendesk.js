00adaef3115bba9a6473463cc71281b449940eba
moderatorwes <wes@cibertec.net>
2016-02-08 10:13:32 -0500

    initial commit
    Add the JS to the JS tab - the code should go right below the $(document).ready(function() {  
    
    **Make sure you change the "yourdomain" in the JS file

---
+++ zendesk.js
@@ -0,0 +1,15 @@
+  // MW-Notification Banner
+   $.get( "https://yourdomin.zendesk.com/api/v2/help_center/articles.json?label_names=alert" ).done(function( data ) {
+    console.log(data);
+   $.each(data.articles, function(index,item) {
+     console.log(item);
+     
+     var style1 = '<div class="ns-box ns-bar ns-effect-slidetop ns-type-notice ns-show"><div class="ns-box-inner"><span class="megaphone"></span></i><p><a href="'+ item.html_url + '">' + item.title + '</a>' + item.body + '</p></div><span class="ns-close"></span></div>'
+           
+     $('.alertbox').append(style1);
+   });
+   $('.ns-close').on('click',function(){
+    $(".alertbox").remove();
+  });
+    
+  });
